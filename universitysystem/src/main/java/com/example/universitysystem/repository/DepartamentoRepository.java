22d60893e5b61dfea6a7086433a3444f6547dd58
joaopsantos04 <joao.pedro@maisunifacisa.com.br>
2024-11-26 00:49:05 -0300

    Commit

--- universitysystem/src/main/java/com/example/universitysystem/repository/DepartamentoRepository.java
+++ universitysystem/src/main/java/com/example/universitysystem/repository/DepartamentoRepository.java
@@ -1,9 +1,13 @@
 package com.example.universitysystem.repository;
 
 import com.example.universitysystem.model.Curso;
+import com.example.universitysystem.model.Departamento;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DepartamentoRepository extends JpaRepository<Curso, Long>{
+
+	Departamento save(Departamento departamento);
 	
 
 }
